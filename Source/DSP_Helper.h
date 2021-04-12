52faa566d772ef0df9b38d8a66af01d10b9c08bb
Zeno Lösch <54742442+CodeZenAudio@users.noreply.github.com>
2021-04-12 20:26:06 +0200

    Add files via upload

---
+++ Source/DSP_Helper.h
@@ -0,0 +1,56 @@
+/*
+  ==============================================================================
+
+    DSP_Helper.h
+    Created: 11 Feb 2021 5:28:42pm
+    Author:  Zeno  Loesch
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#define TWOPI 6.283185307179586476925286766559
+inline float linear_interp(float const v0, float const v1, float const t )
+{
+    return (1 - t) * v0 + t * v1;
+}
+
+
+inline double bpm_to_herz (double const bpm, double const timesignatureindex)
+{
+    double result;
+    if (timesignatureindex == 0)
+    {
+        result = 60;
+        
+    } else if (timesignatureindex == 1)
+    {
+        result = 30;
+       
+    } else if (timesignatureindex == 2)
+    {
+        result = 15;
+  
+    } else if (timesignatureindex == 3)
+    {
+        result = 7.5;
+  
+    } else if (timesignatureindex == 4)
+    {
+        result = 3.75;
+   
+    } else if (timesignatureindex == 5)
+    {
+        result = 1.975;
+   
+    } else if (timesignatureindex == 6)
+    {
+        result = 0.9875;
+     
+    } else
+    {
+        return (1000.0f);
+    }
+       return (bpm / static_cast<double>(result));
+}
---
+++ Source/PluginProcessor.h
@@ -0,0 +1,76 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+#include "Sine.h"
+
+#include "AR.h"
+//==============================================================================
+/**
+*/
+class AMSNYCAudioProcessor  : public juce::AudioProcessor
+{
+public:
+    //==============================================================================
+    AMSNYCAudioProcessor();
+    ~AMSNYCAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+    juce::AudioProcessorValueTreeState treeState;
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    
+    
+    
+private:
+    juce::AudioPlayHead* playhead;
+    juce::AudioPlayHead::CurrentPositionInfo hostInfo;
+    
+    double m_frequency;
+    static const int kChannels = 2;
+    double SampleRate;
+    Sine sineprocess[kChannels];
+    
+    AR *env = new AR();
+ 
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AMSNYCAudioProcessor)
+};
